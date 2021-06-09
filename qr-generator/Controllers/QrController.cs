18bbe1d1aee6747dc4c96f4d240168752ad2fa4a
claudiopedalino <claudio.dpedalino@gmail.com>
2021-06-09 07:56:40 -0300

    Add project files.

---
+++ qr-generator/Controllers/QrController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+[ApiController]
+[Route("[controller]")]
+public class QrController : ControllerBase
+{
+    private readonly ILogger<QrController> _logger;
+
+    public QrController(ILogger<QrController> logger)
+    {
+        _logger = logger;
+    }
+
+
+    [HttpGet("generate/{data}")]
+    public IActionResult GetByUrl([FromRoute] string data) 
+        => string.IsNullOrEmpty(data) 
+            ? BadRequest("No data given") 
+            : File(QrCodeGenerator.GenerateByteArray(data), "image/jpeg");
+}
---
+++ qr-generator/Services/QrCodeGenerator.cs
@@ -0,0 +1,21 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.IO;
+
+public static class QrCodeGenerator
+{
+    public static byte[] GenerateByteArray(string url) 
+        => ImageToByte(GenerateImage(url));
+
+    public static Bitmap GenerateImage(string url) 
+        => new QRCode(
+            new QRCodeGenerator().CreateQrCode(url, QRCodeGenerator.ECCLevel.Q))
+            .GetGraphic(10);
+
+    private static byte[] ImageToByte(Image img)
+    {
+        using var stream = new MemoryStream();
+        img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+        return stream.ToArray();
+    }
+}
